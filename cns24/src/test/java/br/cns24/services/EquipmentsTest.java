f55c7c2b216d8150fb1a0262fb4862945133d1e1
elntele <elntele@gmail.com>
2024-09-03 09:22:41 +0000

    add tests to getLinkPosition

--- cns24/src/test/java/br/cns24/services/EquipmentsTest.java
+++ cns24/src/test/java/br/cns24/services/EquipmentsTest.java
@@ -78,4 +78,22 @@ public class EquipmentsTest {
       }
     }
   }
+
+  @Test
+  public void shouldReturnTheCorrectPositionWhenNumNodeIsFiveAndSetIsOneFiber() {
+    Integer[] matrixConnection = new Integer[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
+    int sizeSet = 5;
+    int numNodes = 4;
+    int counter = 0;
+    for (int i = 0; i < numNodes; i++) {
+      for (int j = i + 1; j < numNodes; j++) {
+        var matrixIndex = Equipments.getLinkPosition(i, j, numNodes, sizeSet);
+        int valueInMatrix = matrixConnection[matrixIndex];
+        if (counter <= matrixConnection.length - sizeSet) {
+          assertEquals(valueInMatrix, counter);
+        }
+        counter += 5;
+      }
+    }
+  }
 }
